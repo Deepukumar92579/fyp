d75ff472e561715d182b8632eb825371a0c1bbea
Deepukumar92579 <deepukumar92579@gmail.com>
2025-09-19 15:16:42 +0530

    Update Main_Model.py

--- Main_Model.py
+++ Main_Model.py
@@ -99,7 +99,7 @@ model = AutoModelForTokenClassification.from_pretrained(
 # Training arguments
 training_args = TrainingArguments(
     output_dir="./results",
-    evaluation_strategy="epoch",
+    eval_strategy="epoch",
     save_strategy="epoch",
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
